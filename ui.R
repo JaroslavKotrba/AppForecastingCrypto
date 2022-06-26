44582aa17fa43722ead50ec73b02ceab52c26a9c
JaroslavKotrba <65077222+JaroslavKotrba@users.noreply.github.com>
2022-06-26 20:41:13 +0200

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,58 @@
+# Libraries
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(plotly)
+
+# ui
+ui <- fluidPage(
+  title = "Crypto Currencies",
+  titlePanel("Crypto Currencies"),
+  br(),
+  p("See crypto currencies prices, select the one you are interested in from the box below!"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      selectInput(
+        inputId = "select_coin",
+        label = "Select crypto currency:",
+        choices = c("BTC-USD", "ETH-USD", "CRO-USD"),
+        selected = "BTC-USD"
+      ),
+      
+      dateRangeInput(
+        inputId = "date",
+        label = "Select date:",
+        start = "2020-01-01", 
+        end = as.character(Sys.Date())
+      ),
+      
+      numericInput(
+        inputId = "periods",
+        label = "Select number of forecasted days:",
+        value = 90,
+        min = 0, max = 365, step = 5
+      ),
+      
+      selectInput(
+        inputId = "breaks",
+        label = "Select date breaks:",
+        choices = c("1 month", "4 months", "6 months", "12 months"),
+        selected = "6 months"
+      )
+    ),
+    
+    mainPanel(
+      
+      # Title of the graph
+      verbatimTextOutput("name"),
+      
+      plotlyOutput("coin"), # plotly
+    )
+  ),
+  
+  p("\n"),
+  uiOutput("link1", style="padding-left: 0px"),
+  uiOutput("link2", style="padding-left: 0px")
+)
